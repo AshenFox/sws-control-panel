ad11d745f5aa8cd0621b47a19dab23aa3ea76c54
Max_K <kavokinm@gmail.com>
2022-11-03 15:58:22 +0300

    Api url bug.

--- src/helpers/values.ts
+++ src/helpers/values.ts
@@ -1,2 +1,2 @@
 export const token = '2482';
-export const api_url = 'http://185.244.172.108:8081/v1/outlay-rows/entity/';
+export const api_url = 'https://185.244.172.108:8081/v1/outlay-rows/entity/';
